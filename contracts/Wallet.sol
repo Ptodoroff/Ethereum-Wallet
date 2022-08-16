e1fadadb0cdc5e3b32d6526c013b0630957ceaff
Petar <p.todorov212@gmail.com>
2022-08-16 14:06:02 +0300

    added contract fns()

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -2,15 +2,29 @@ pragma solidity 0.8.15;
 
 contract Wallet {
 
+    address owner;
+
+
+  constructor () {
+    owner = msg.sender;
+  }
+    modifier onlyOwner {
+        require (msg.sender ==owner);
+        _;
+    }
 
     function deposit () external payable {
 
     }
 
-    function transfer (address payable _to, uint _amount) external {
+    function transfer (address payable _to, uint _amount) external onlyOwner {
         _to.transfer(_amount);
+
     }
 
+    function walletBalance () public view returns (uint) {
+        return address(this).balance;
+    }
 
 
 
